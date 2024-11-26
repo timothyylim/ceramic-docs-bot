e49d4bf4e2f19149efa507c2767aac59b21f5906
timothyylim <timothylim23@gmail.com>
2024-11-26 16:06:16 +0800

    set max tokens

--- src/index.ts
+++ src/index.ts
@@ -125,6 +125,7 @@ discordClient.on('messageCreate', async (message: Message) => {
             {
                 model: 'o1-mini', // Ensure you are using a compatible model
                 messages: truncatedHistory,
+                max_tokens: 500,
             },
             {
                 headers: {
